8969d6414f2ebef7227beb9c14b6b57b433c5121
parzivald <mukhammadkodirmakhmudjanov@gmail.com>
2023-08-13 16:35:57 +0500

    another final

--- data/config.py
+++ data/config.py
@@ -1,15 +1,20 @@
-from environs import Env
+# from environs import Env
+#
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
+#
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
+#
+# DB_USER = env.str("DB_USER")
+# DB_PASS = env.str("DB_PASS")
+# DB_NAME = env.str("DB_NAME")
+# DB_HOST = env.str("DB_HOST")
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
-
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
-
-DB_USER = env.str("DB_USER")
-DB_PASS = env.str("DB_PASS")
-DB_NAME = env.str("DB_NAME")
-DB_HOST = env.str("DB_HOST")
+import os 
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
+ADMINS = list(os.environ.get("ADMINS"))
+IP = str(os.environ.get("ip"))
