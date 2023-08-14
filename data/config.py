672eb133fba58c0ff02027d4aab01a6f5a814e7b
parzivald <mukhammadkodirmakhmudjanov@gmail.com>
2023-08-14 20:52:46 +0500

    final deploy edit

--- data/config.py
+++ data/config.py
@@ -18,3 +18,8 @@ import os
 BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
 ADMINS = list(os.environ.get("ADMINS"))
 IP = str(os.environ.get("ip"))
+
+DB_USER = str(os.environ.get("DB_USER"))
+DB_PASS = str(os.environ.get("DB_PASS"))
+DB_NAME = str(os.environ.get("DB_NAME"))
+DB_HOST = str(os.environ.get("DB_HOST"))
