cfae55d84261a63487809923f795058129c14258
parzivald <mukhammadkodirmakhmudjanov@gmail.com>
2023-08-15 13:31:48 +0500

    alright then

--- data/config.py
+++ data/config.py
@@ -14,12 +14,12 @@
 # DB_NAME = env.str("DB_NAME")
 # DB_HOST = env.str("DB_HOST")
 
-import os 
+import os
 BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
-ADMINS = list(os.environ.get("ADMINS"))
+ADMINS = str(os.environ.get("ADMINS"))
 # IP = str(os.environ.get("ip"))
 
-DB_USER = str(os.environ.get("DB_USER"))
-DB_PASS = str(os.environ.get("DB_PASS"))
-DB_NAME = str(os.environ.get("DB_NAME"))
-DB_HOST = str(os.environ.get("DB_HOST"))
+# DB_USER = str(os.environ.get("DB_USER"))
+# DB_PASS = str(os.environ.get("DB_PASS"))
+# DB_NAME = str(os.environ.get("DB_NAME"))
+# DB_HOST = str(os.environ.get("DB_HOST"))
