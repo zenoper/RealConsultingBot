d90afeb079f47a107291e1e0c12238c2b3acac70
parzivald <mukhammadkodirmakhmudjanov@gmail.com>
2023-11-08 12:08:47 +0500

    RealConsultingBot 2.0

--- data/config.py
+++ data/config.py
@@ -5,7 +5,7 @@ env = Env()
 env.read_env()
 #
 # # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 # ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 # IP = env.str("ip")  # Xosting ip manzili
 #
@@ -15,7 +15,7 @@ BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 # DB_HOST = env.str("DB_HOST")
 
 import os
-# BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
 ADMINS = str(os.environ.get("ADMINS"))
 # IP = str(os.environ.get("ip"))
 
