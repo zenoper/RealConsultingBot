ae43377d145a22b3694d107af022aaea3efbdc1c
Parzivald <bez@Muhammadqodirs-Mac.local>
2024-02-06 09:17:06 +0500

    final deploy

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ from utils.set_bot_commands import set_default_commands
 
 async def on_startup(dispatcher):
     await db.create()
-    await db.drop_users()
-    await db.drop_b1users()
-    await db.drop_videos()
+    # await db.drop_users()
+    # await db.drop_b1users()
+    # await db.drop_videos()
     await db.create_table_users()
     await db.create_table_b1users()
     await db.create_table_videos()
